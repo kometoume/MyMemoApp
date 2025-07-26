4d87d2a1eeb359c9a6eb7703be803c09183cc1f6
Michie Yagi <kometoume@gmail.com>
2025-07-26 18:19:22 +0900

    localstragedatedel

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 {
   const text = document.getElementById('text');
   const save = document.getElementById('save');
+  const clear = document.getElementById('clear');
   const message = document.getElementById('message');
 
   if (localStorage.getItem('memo') === null) {
@@ -17,5 +18,12 @@
       message.classList.remove('appear');
     }, 1000);
     localStorage.setItem('memo', text.value);
+  });
+
+  clear.addEventListener('click', () => {
+    if (confirm('本当に削除しますか？') === true) {
+      text.value = '';
+      localStorage.removeItem('memo');
+    }
   });
 }
