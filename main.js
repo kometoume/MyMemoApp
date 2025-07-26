8f134efdf86d930397448f83f715034ecd3a2ca6
Michie Yagi <kometoume@gmail.com>
2025-07-26 16:27:04 +0900

    keepbutton

--- main.js
+++ main.js
@@ -3,8 +3,13 @@
 {
   const text = document.getElementById('text');
   const save = document.getElementById('save');
+  const message = document.getElementById('message');
 
   save.addEventListener('click', () => {
+    message.classList.add('appear');
+    setTimeout(() => {
+      message.classList.remove('appear');
+    }, 1000);
     localStorage.setItem('memo', text.value);
   });
 }
